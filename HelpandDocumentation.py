5e6da0943548cd533d8a658348343f215490f343
Yogesh Daksh <Yogeshdaksh606@gmail.com>
2024-10-26 17:52:43 +0530

    U1 P6

---
+++ HelpandDocumentation.py
@@ -0,0 +1,10 @@
+# Program 6: Help and Documentation
+
+# Use help() to display documentation for a built-in module, like math
+import math
+print("Documentation for math module:")
+help(math)
+
+# Alternatively, you can get help for a specific function
+print("\nDocumentation for math.sqrt function:")
+help(math.sqrt)
