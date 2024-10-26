ceb2ea8624647140aa9c985ad583f9d752ea3e7c
Yogesh Daksh <Yogeshdaksh606@gmail.com>
2024-10-26 17:51:53 +0530

    U1 P1

---
+++ DynamicTypingDemonstration.py
@@ -0,0 +1,17 @@
+# Program 1: Dynamic Typing Demonstration
+
+# Assign an integer value
+variable = 10
+print("Value:", variable, "| Type:", type(variable))
+
+# Reassign a float value
+variable = 10.5
+print("Value:", variable, "| Type:", type(variable))
+
+# Reassign a string value
+variable = "Hello, Python!"
+print("Value:", variable, "| Type:", type(variable))
+
+# Reassign a list
+variable = [1, 2, 3, 4]
+print("Value:", variable, "| Type:", type(variable))
