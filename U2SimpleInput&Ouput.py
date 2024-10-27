5345756f2588db2592167a21c7d796c271f41788
Yogesh Daksh <Yogeshdaksh606@gmail.com>
2024-10-27 10:09:36 +0530

    Input and Output

---
+++ U2SimpleInput&Ouput.py
@@ -0,0 +1,7 @@
+#Program 14: Simple Input and Ouput
+
+take = input("Enter a Integer number: ") # Input function take input as a string 
+print(type(take)) #print fuction use to display output
+
+num = int(take) #convert string input into integer
+print(type(num))
