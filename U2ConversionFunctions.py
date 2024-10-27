d88a0f88e58e94d0938510f1349b3a19794884c8
Yogesh Daksh <Yogeshdaksh606@gmail.com>
2024-10-27 10:09:18 +0530

    convert a variable from a type to another

---
+++ U2ConversionFunctions.py
@@ -0,0 +1,18 @@
+#Program 13: Conversion Functions
+
+num1 = "12"
+num2= 12.2
+
+print(num1, num2)
+
+print(type(num1))
+print(type(num2))
+
+num1_conversion = float(num1) #convert string to float
+num2_conversion = int(num2) #convert float to interger
+
+print(num1_conversion,num2_conversion)
+
+print(type(num1_conversion))
+print(type(num2_conversion))
+
