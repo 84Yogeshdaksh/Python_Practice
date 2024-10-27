ffecb2ed29ca3a4209ddf4a7ad8e1e6db369845e
Yogesh Daksh <Yogeshdaksh606@gmail.com>
2024-10-27 10:10:01 +0530

    String Slicing

---
+++ U2StringSlicing.py
@@ -0,0 +1,16 @@
+#Program 16: String Slicing and len() function
+
+P = "YogeshDaksh"
+
+print(len(P))
+
+print(P[:5]) #print from index 0 to 4
+
+print(P[2:]) #print from index 2 to end
+
+print(P[:]) #print from index 0 to end
+
+print(P[0:-1]) #print from index 0 to -2(second last)
+
+print(P[-1:]) #print from index end to .....
+
