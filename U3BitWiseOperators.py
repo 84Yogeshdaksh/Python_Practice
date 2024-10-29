618f78f1be676375e793e38627801cd548857ec9
Yogesh Daksh <Yogeshdaksh606@gmail.com>
2024-10-29 10:26:49 +0530

    Bitwise Operators

---
+++ U3BitWiseOperators.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter Second number: "))
+
+#Bitwise Operators(&,|,~,^,>>,<<)
+print("a & b", a & b)
+print("a | b", a | b)
+print(" ~b",  ~b)
+print("a ^ b", a ^ b)
+print ("a<<", a<<1)
+print("b>>", b>>1)
