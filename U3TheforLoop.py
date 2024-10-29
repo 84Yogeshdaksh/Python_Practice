9c7094b3511496a095994386ba81bb87bccc7905
Yogesh Daksh <Yogeshdaksh606@gmail.com>
2024-10-29 11:15:03 +0530

    The for loop

---
+++ U3TheforLoop.py
@@ -0,0 +1,21 @@
+a=2
+for i in range(11):
+    if i==0:
+        continue
+    print(i*a)
+
+
+a="computer"
+b=input("Enter a sun string")
+if b in a:
+    print("True")
+else:
+    print("False")
+
+a=["Yogesh","YeS","x","Hunter"]
+
+for a2 in range(10):
+    print(a2)
+    
+for a2 in a:
+    print(a2)
